34dfb2eb381049715770c1d2b4ba26a1e90bf20f
kevarey reynolds <kevareynolds@gmail.com>
2021-03-01 09:34:04 -0600

    first commit

---
+++ api/src/main/java/com/tts/api/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.tts.api.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/v1/hello")
+    public String getHello(){
+        return "Hello Friends";
+    }
+}
---
+++ api/src/test/java/com/tts/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
