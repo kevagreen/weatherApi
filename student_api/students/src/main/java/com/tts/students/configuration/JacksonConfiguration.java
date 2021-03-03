5f8145bb21c6a2934ef65ca156a990a3c033f594
kevagreen <kevareynolds@gmail.com>
2021-03-03 17:17:25 -0600

    Added Jackson library to use @JsonFormatter for localDate

---
+++ student_api/students/src/main/java/com/tts/students/configuration/JacksonConfiguration.java
@@ -0,0 +1,21 @@
+package com.tts.students.configuration;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationConfig;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfiguration  {
+
+    @Bean
+    public ObjectMapper createObjectMapper(){
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
+    }
+}
